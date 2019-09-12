9ded2fda483ee77ef381d109c70673b19b3a3e77
Pedro Façanha <pedrofacpeix@gmail.com>
2019-09-12 14:26:56 -0300

    Second commit

---
+++ Reviewster/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Reviewster
+//
+//  Created by aluno on 12/09/19.
+//  Copyright © 2019 aluno. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Reviewster/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Reviewster
+//
+//  Created by aluno on 12/09/19.
+//  Copyright © 2019 aluno. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
